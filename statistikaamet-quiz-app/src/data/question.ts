081abf08a06dd5410dce1041aa84fc05ad1a4b65
Agnessa Tund <agnessa.tund@gmail.com>
2026-03-09 22:15:08 +0200

    feat: add quiz question dataset

---
+++ statistikaamet-quiz-app/src/data/question.ts
@@ -0,0 +1,28 @@
+export type Question = {
+  question: string;
+  options: string[];
+  correctAnswer: number;
+};
+
+export const questions: Question[] = [
+  {
+    question: "Milline saar on pindalalt Eesti suurim?",
+    options: ["Hiiumaa", "Kihnu", "Vormsi", "Saaremaa"],
+    correctAnswer: 3,
+  },
+  {
+    question: "Mitu maakonda on Eestis?",
+    options: ["15", "10", "18", "20"],
+    correctAnswer: 0,
+  },
+  {
+    question: "Milline linn on tuntud kui Eesti suvepealinn?",
+    options: ["Kuressaare", "Pärnu", "Rakvere", "Tallinn"],
+    correctAnswer: 1,
+  },
+  {
+    question: "Milline neist on Eesti rahvuslill?",
+    options: ["Roos", "Tulp", "Rukkilill", "Nartsiss"],
+    correctAnswer: 2,
+  },
+];
