490952073724b837b0100a39786f6eac78413fe5
Agnessa Tund <agnessa.tund@gmail.com>
2026-03-10 10:16:37 +0200

    feat: add ResultTable component for displaying quiz results

---
+++ statistikaamet-quiz-app/src/components/ResultTable.tsx
@@ -0,0 +1,29 @@
+import type { Result } from "../types/result";
+
+type Props = {
+  results: Result[];
+};
+
+export default function ResultTable({ results }: Props) {
+  return (
+    <table border={1}>
+      <thead>
+        <tr>
+          <th>Küsimus</th>
+          <th>Sinu vastus</th>
+          <th>Tulemus</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {results.map((r, index) => (
+          <tr key={index}>
+            <td>{r.question}</td>
+            <td>{r.selectedAnswer}</td>
+            <td>{r.isCorrect ? "Õige" : "Vale"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
